55a62af0e65e6f1554a3967f7ac8fd83093e9bb5
Josh Kellendonk <josh@colloco.com>
2019-10-11 14:04:53 -0600

    Add editorconfig, scripts, re-save existing files with new editorconfig

---
+++ scripts/packages.sh
@@ -0,0 +1,19 @@
+
+# Fri Oct 11 14:02:50 MDT 2019
+apt install whois
+apt install kubuntu-desktop --no-install-recommends
+apt-get install dkms
+apt install sddm
+apt install mesa-utils
+apt install konsole
+apt install php7.2-cli
+apt install php-xdebug
+apt install php7.2-zip
+apt install php7.2-yaml
+apt install apt-fast
+apt install cmake
+apt install zeal
+apt install lastpass-cli
+apt install php7.2-mbstring php7.2-xml
+apt install samba
+apt install cmatrix
---
+++ scripts/refresh-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+HERE=$(dirname $0)
+PACKAGES_SH=$HERE/packages.sh
+
+if [ -f "$PACKAGES_SH" ]; then
+  # Comment out the old commands.
+  sed -ri 's/^([^#])/# \1/' $PACKAGES_SH
+fi
+
+echo -e "\n# $(date)" >>$PACKAGES_SH
+zegrep "apt(-get)? (install|remove)" /var/log/apt/history* | sed 's/.*Commandline: //' >>$PACKAGES_SH
+chmod +x $PACKAGES_SH
