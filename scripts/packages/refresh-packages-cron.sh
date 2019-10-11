208e1cf8a5b667d3aa68a7aa63f1545d01735b34
Josh Kellendonk <josh@colloco.com>
2019-10-11 14:46:40 -0600

    Use $HERE for git add

--- scripts/packages/refresh-packages-cron.sh
+++ scripts/packages/refresh-packages-cron.sh
@@ -7,7 +7,7 @@ HERE=$(dirname $0)
 git stash push >/dev/null
 
 $HERE/refresh-packages.sh >/dev/null
-git add packages.sh >/dev/null
+git add $HERE/packages.sh >/dev/null
 git commit -m "Packages updated on $(date)" >/dev/null
 git push origin master >/dev/null
 
