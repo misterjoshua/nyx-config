26dbb5fa3fa60d4a2dcc3977bd006a187c322a00
Josh Kellendonk <josh@colloco.com>
2019-10-11 14:34:36 -0600

    Packages updated on Fri Oct 11 14:34:36 MDT 2019

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,19 +1,56 @@
 
 # Fri Oct 11 14:22:01 MDT 2019
+# apt install whois
+# apt install kubuntu-desktop --no-install-recommends
+# apt-get install dkms
+# apt install sddm
+# apt install mesa-utils
+# apt install konsole
+# apt install php7.2-cli
+# apt install php-xdebug
+# apt install php7.2-zip
+# apt install php7.2-yaml
+# apt install apt-fast
+# apt install cmake
+# apt install zeal
+# apt install lastpass-cli
+# apt install php7.2-mbstring php7.2-xml
+# apt install samba
+# apt install cmatrix
+# snap install --channel=stable --classic code
+# snap install --channel=stable docker
+# snap install --channel=stable firefox
+# snap install --channel=stable gimp
+# snap install --channel=stable gtk-common-themes
+# snap install --channel=stable --classic helm
+# snap install --channel=stable --classic intellij-idea-ultimate
+# snap install --channel=stable jq
+# snap install --channel=stable --classic kubectl
+# snap install --channel=stable --classic microk8s
+# snap install --channel=10 --classic node
+# snap install --channel=stable spotify
+# snap install --channel=stable tldr
+# snap install --channel=stable travis
+
+# Fri Oct 11 14:34:36 MDT 2019
 apt install whois
 apt install kubuntu-desktop --no-install-recommends
 apt-get install dkms
 apt install sddm
 apt install mesa-utils
+apt install kterm
 apt install konsole
+apt remove kterm
 apt install php7.2-cli
 apt install php-xdebug
+apt install phpunit
 apt install php7.2-zip
 apt install php7.2-yaml
 apt install apt-fast
 apt install cmake
 apt install zeal
 apt install lastpass-cli
+apt remove phpunit
 apt install php7.2-mbstring php7.2-xml
 apt install samba
 apt install cmatrix
@@ -21,6 +58,7 @@ snap install --channel=stable --classic code
 snap install --channel=stable docker
 snap install --channel=stable firefox
 snap install --channel=stable gimp
+snap install --channel=1.13 --classic go
 snap install --channel=stable gtk-common-themes
 snap install --channel=stable --classic helm
 snap install --channel=stable --classic intellij-idea-ultimate
